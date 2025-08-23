5d66b4355bef2722086e591d1e3f34a38a4ad27a
HiwaAbdolahi <89581205+HiwaAbdolahi@users.noreply.github.com>
2025-08-23 16:52:33 +0200

    Remove “scroll to top”: delete button, JS handlers and CSS; fix classList null error !

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -4,14 +4,6 @@
 
 const elemToggleFunc = function (elem) { elem.classList.toggle('active'); }
 
-// Header Sticky & Go-Top
-
-const header = document.querySelector('[data-header]');
-const goTopBtn = document.querySelector('[data-go-top]');
-window.addEventListener('scroll', function () {
-    if (window.scrollY >= 10) { header.classList.add('active'); goTopBtn.classList.add('active'); }
-    else { header.classList.remove('active'); goTopBtn.classList.remove('active'); }
-});
 
 // Mobile Menu
 
